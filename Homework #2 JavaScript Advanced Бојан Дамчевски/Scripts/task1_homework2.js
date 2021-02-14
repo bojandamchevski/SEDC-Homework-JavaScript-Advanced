588698518f8f610ee2c69dac5927255074716775
bojandamchevski <bojandamcevski@gmail.com>
2021-02-14 20:35:19 +0100

    update

--- Homework #2 JavaScript Advanced Бојан Дамчевски/Scripts/task1_homework2.js
+++ Homework #2 JavaScript Advanced Бојан Дамчевски/Scripts/task1_homework2.js
@@ -1,27 +1,36 @@
 $(document).ready(function () {
     let button = $("#btn");
     let body = $("body");
-    button.click(() => {
-        button.hide();
-        body.append("<table id='table1'>");
-        let table = $("table");
-        table.append("<thead>");
-        table.append("<tbody>");
-        let tableHead = $("thead");
-        let tableBody = $("tbody");
-        tableHead.append("<tr><td>Planet Name</td><td>Population</td><td>Climate</td><td>Gravity</td></tr>");
+    body.append("<table id='table1'>");
+    let table = $("table");
+    table.append("<thead>");
+    table.append("<tbody>");
+    let tableHead = $("thead");
+    let tableBody = $("tbody");
+    table.hide();
+    tableHead.append("<tr><td>Planet Name</td><td>Population</td><td>Climate</td><td>Gravity</td></tr>");
+    let callApi = (url) => {
+        table.show();
         $.ajax({
             method: "GET",
-            url: "https://swapi.dev/api/planets/?page=1",
+            url: url,
             success: (result) => {
-                for(let item of result.results){
-                    tableBody.append(`<tr><td>${item.name}</td><td>${item.population}</td><td>${item.climate}</td><td>${item.gravity}</td></tr>`);
-                }
+                print(result);
             },
-            error: (result) =>{
+            error: (result) => {
                 console.log("An ERROR has occured");
                 console.log(result);
             }
         });
+
+        let print = (data) => {
+            for (let item of data.results) {
+                tableBody.append(`<tr><td>${item.name}</td><td>${item.population}</td><td>${item.climate}</td><td>${item.gravity}</td></tr>`);
+            }
+        }
+    }
+    button.click(() => {
+        button.hide();
+        callApi("https://swapi.dev/api/planets/?page=1");
     });
 });
