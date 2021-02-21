1fb7d77c55ae7f7860931d437cd6c2fbc7be4a75
bojandamchevski <bojandamcevski@gmail.com>
2021-02-21 19:07:35 +0100

    update

--- Homework #3 JavaScript Advanced Бојан Дамчевски/Scripts/task1_homework3.js
+++ Homework #3 JavaScript Advanced Бојан Дамчевски/Scripts/task1_homework3.js
@@ -23,47 +23,78 @@ let products = [
     new Product("Speakers", "Technology", true, 22000)
 ];
 
-console.log("===== Price greater than 20 =====");
-let productsWithPriceGreaterThan20 = products.filter(product => product.price > 20);
-let productsWithPriceGreaterThan20Map = productsWithPriceGreaterThan20.map(p => p.name);
-console.log(productsWithPriceGreaterThan20);
-console.log(productsWithPriceGreaterThan20Map);
-productsWithPriceGreaterThan20.forEach(p => console.log(p.name));
+console.log("1");
+let allProdutsWithPriceAbove20 = products.filter(p=>p.price>20);
+allProdutsWithPriceAbove20.forEach(p=>console.log(`${p.name} ${p.price}`));
+console.log(allProdutsWithPriceAbove20);
 
-console.log("===== Category Food on discount =====");
-let foodProducts = products.filter(product => product.category == "Food" && product.hasDiscount === true);
-let foodProductsMap = foodProducts.map(p => p.name)
-console.log(foodProducts);
-console.log(foodProductsMap);
-foodProducts.forEach(p => console.log(p.name));
+console.log("2");
+let allProductsOnDiscount = products.filter(p => p.hasDiscount === true);
+allProductsOnDiscount.forEach(p=>console.log(`${p.name} ${p.price}`));
 
+console.log("3");
+let discountMap = allProductsOnDiscount.map(p => p.price);
+let sumOfPrice = discountMap.reduce((sum, price) => sum += price, 0);
+let averagePrice = sumOfPrice / discountMap.length;
+console.log(averagePrice);
 
-console.log("===== Average price of all products that are on discount =====");
-function averagePrice(sum, price) {
-    return sum += price;
+console.log("4");
+let notOnDiscount = products.filter(p=>p.hasDiscount === false);
+let notOnDiscountMap = notOnDiscount.map(p=>`${p.name} ${p.price}`);
+console.log(notOnDiscountMap);
+let startWithVowel = notOnDiscountMap.filter(name=>/^[aeiou]/i.test(name),0);
+console.log(startWithVowel);
+
+console.log("5");
+let copiedArray = [];
+for(let i = 0; i < products.length; i++){
+    copiedArray.push(products[i]);
 }
 
-let productsOnDiscount = products.filter(product => product.hasDiscount === true);
-let productsOnDiscountMap = productsOnDiscount.map(p => p.price)
-    .reduce(averagePrice, 0);
-let average = productsOnDiscountMap / productsOnDiscount.length;
-console.log(average);
+// let sortedAscending = copiedArray.sort((a,b)=>a.price - b.price)
+// .forEach(p=>console.log(`${p.name} ${p.price}`));
 
-console.log("===== Products which name starts with a vowel =====");
-let productsNotOnDiscount = products.filter(product => product.hasDiscount === false).map(product => product.name);
-console.log(productsNotOnDiscount);
-function startsWithVowel(word) {
-    console.log(productsNotOnDiscount.filter(p => /^[aeiou]/i.test(p)));
-}
-startsWithVowel(productsNotOnDiscount);
+// console.log("===== Price greater than 20 =====");
+// let productsWithPriceGreaterThan20 = products.filter(product => product.price > 20);
+// let productsWithPriceGreaterThan20Map = productsWithPriceGreaterThan20.map(p => p.name);
+// console.log(productsWithPriceGreaterThan20);
+// console.log(productsWithPriceGreaterThan20Map);
+// productsWithPriceGreaterThan20.forEach(p => console.log(p.name));
 
-console.log("===== Sort the products by price ascending =====");
-function copyOfArray(arr) {
-    let newArray = [];
-    arr.forEach(product => newArray.push(product));
-    return newArray;
-}
-let newArrayCopy = copyOfArray(products);
-newArrayCopy.sort((p1, p2) => p1.price - p2.price);
-console.log(newArrayCopy);
-newArrayCopy.forEach(p => console.log(`${p.name} ${p.price}`));
+// console.log("===== Category Food on discount =====");
+// let foodProducts = products.filter(product => product.category == "Food" && product.hasDiscount === true);
+// let foodProductsMap = foodProducts.map(p => p.name)
+// console.log(foodProducts);
+// console.log(foodProductsMap);
+// foodProducts.forEach(p => console.log(p.name));
+
+
+// console.log("===== Average price of all products that are on discount =====");
+// function averagePrice(sum, price) {
+//     return sum += price;
+// }
+
+// let productsOnDiscount = products.filter(product => product.hasDiscount === true);
+// let productsOnDiscountMap = productsOnDiscount.map(p => p.price)
+//     .reduce(averagePrice, 0);
+// let average = productsOnDiscountMap / productsOnDiscount.length;
+// console.log(average);
+
+// console.log("===== Products which name starts with a vowel =====");
+// let productsNotOnDiscount = products.filter(product => product.hasDiscount === false).map(product => product.name);
+// console.log(productsNotOnDiscount);
+// function startsWithVowel(word) {
+//     console.log(productsNotOnDiscount.filter(p => /^[aeiou]/i.test(p)));
+// }
+// startsWithVowel(productsNotOnDiscount);
+
+// console.log("===== Sort the products by price ascending =====");
+// function copyOfArray(arr) {
+//     let newArray = [];
+//     arr.forEach(product => newArray.push(product));
+//     return newArray;
+// }
+// let newArrayCopy = copyOfArray(products);
+// newArrayCopy.sort((p1, p2) => p1.price - p2.price);
+// console.log(newArrayCopy);
+// newArrayCopy.forEach(p => console.log(`${p.name} ${p.price}`));
